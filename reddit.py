aff1673358b73e389603ce73a1de4366c72f0292
David Hazinski <dhazinski@gmail.com>
2011-11-05 23:55:31 -0400

    Adding favicon handler.

--- reddit.py
+++ reddit.py
@@ -3,13 +3,20 @@
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+FAVICON_URL = 'http://www.reddit.com/favicon.ico'
+
 class MainHandler(webapp.RequestHandler):
 
     def get(self):
         return
 
+class FaviconHandler(webapp.RequestHandler):
+    def get(self):
+        self.redirect(FAVICON_URL, permanent=True)
+
 def main():
     urls = [('/?', MainHandler),
+            ('/favicon.ico', FaviconHandler)
             ]
 
     application = webapp.WSGIApplication(urls, debug=True)
